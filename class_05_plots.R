45157121f5c8189cb4537b7d4c295422867018cf
Ronalds <cinksronalds@gmail>
2024-02-04 23:26:56 +0200

    added some colors

---
+++ class_05_plots.R
@@ -0,0 +1,36 @@
+# Plots
+
+
+?islands     # built in vector of the areas in thousands of square miles 
+# of the landmasses which exceed 10,000 square miles.
+
+# the following subsets the islands vector to ten largest landmasses 
+# decreasing order
+largest_islands <- head(sort(islands, decreasing = TRUE), 10)
+
+
+plot(largest_islands)
+
+# main = " "  <- adds a title
+plot(largest_islands, main = "Land area of continents and islands")
+
+# until we put the closing parenthesis R is going to know that the
+# function will continue, so we can put code in another row for readability)
+# the ylab <- parameter can be used to override the y axis label
+plot(largest_islands, main = "Land area of continents and islands",
+     ylab = "Land area in  square miles")
+
+# adds text labels to a plot
+# adj = c(x,y)) <- position from the coordinates
+# x/y = 0.5 (middle)
+text(largest_islands[1:2], labels = names(largest_islands[1:2]), adj = c(-1,0.5))
+text(largest_islands[3:9], labels = names(largest_islands[3:9]), adj = c(-0.2,0.4))
+text(largest_islands[10], labels = names(largest_islands[10]), adj = c(-10.7,0.5))
+
+
+
+
+
+
+
+
