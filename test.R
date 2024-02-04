ca9df668a26f4491e26b79176b84a39005642777
Ronalds <cinksronalds@gmail>
2024-02-04 23:01:15 +0200

    git and github setup added

--- test.R
+++ test.R
@@ -4,3 +4,11 @@ y <- 11
 z <- x + y
 
 print(z)
+
+
+# The bellow code will set up git and github connection with rstudio
+git reset --hard "insert SHA number"
+?use_github
+library(usethis)
+edit_r_environ()
+use_github(protocol = 'https', auth_token = Sys.getenv("GITHUB_PAT"))
