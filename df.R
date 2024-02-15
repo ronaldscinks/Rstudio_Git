bc435dc8c50e23017e30b3152aa1872b9ee7528b
Ronalds Cinks <ronalds.cinks@RBS.lan>
2024-02-15 15:43:36 +0200

    sdd

--- df.R
+++ df.R
@@ -6,7 +6,7 @@ profit <- revenue - expenses
 
 # creating a data frame from vectors
 fin.data<-data.frame(revenue, expenses, profit)
-
+fin.data
 # we can make names for the columns when initializing a DF
 data.frame("rev" = revenue, "exp" = expenses, "prof" = profit)
 
@@ -18,12 +18,48 @@ fin.data <- as.data.frame(t(fin.data))
 colnames(fin.data)
 
 
-# 
+# merging data.frames (adding new data values)
+# they need to have the same variable/column names
 p_revenue<-fin.data$revenue * 1.3
 p_expenses <- fin.data$expenses *0.9
 p_profit <- p_revenue - p_expenses
 
-p_fin.data<-data.frame("revenue" = p_revenue, "expenses"p_expenses, p_profit)
+# setting up a df of predicted financial data and changing their column names
+# to match the names of the current fin.data 
+p_fin.data<-data.frame("revenue" = p_revenue, "expenses" = p_expenses, "profit" =  p_profit)
 
-#
-data.frame(rbind(fin.data,p_fin.data))
+
+fin.data <- data.frame(rbind(fin.data,p_fin.data))
+
+# you can add new columns as well
+# we just act as if they are already there
+# here we add the corresponding months and years
+# notice that the added vectors recycle
+
+fin.data$month <- month.name
+fin.data$year <- c(rep(2023,12), rep(2024,12))
+
+# you can change values as well in
+
+fin.data$year <- c(rep(2022,12), rep(2023,12))
+
+# filter data
+# notice that when accessing a single column it is treated as a vector
+fin.data$year[fin.data$year==2022] <- 2021
+
+fin.data[fin.data$profit > mean(fin.data$profit),]
+
+
+#### task nr. 1
+### using the basketball data
+## construct a data frame where row has data for a single player
+# the first column stores players names (So that each row corresponds to a single player), 
+# the columns after that store mean values 
+# of the following stats across all the seasons: 
+# (for column names use the values within parenthesis e.g. FGA)
+# - FieldGoalAttempts (FGA)
+# - FieldGoals (FG)
+# - Games (games)
+# - MinutesPlayed (minutes)
+# - Salary (salary)
+
---
+++ df_2.R
@@ -0,0 +1,58 @@
+# Importing data into RStudio ------------------
+# Selecting your file
+dbase <- read.csv(file.choose())
+
+# Opening your file with R
+# Working directory
+getwd()
+
+# Windows
+setwd("C:\\Users\\ronlads.cinks\\Desktop\\Working Directory")
+# Macbook
+setwd("/users/ronalds.cinks/Desktop/Working Directory")
+
+dbase <- read.csv("Class 06-Demographic-Data.csv")
+View(dbase)
+
+
+# Vectors with Countries Regions
+Countries_Dataset <- c("Aruba","Afghanistan","Angola","Albania","United Arab Emirates","Argentina","Armenia","Antigua and Barbuda","Australia","Austria","Azerbaijan","Burundi","Belgium","Benin","Burkina Faso","Bangladesh","Bulgaria","Bahrain","Bahamas, The","Bosnia and Herzegovina","Belarus","Belize","Bermuda","Bolivia","Brazil","Barbados","Brunei Darussalam","Bhutan","Botswana","Central African Republic","Canada","Switzerland","Chile","China","Cote d'Ivoire","Cameroon","Congo, Rep.","Colombia","Comoros","Cabo Verde","Costa Rica","Cuba","Cayman Islands","Cyprus","Czech Republic","Germany","Djibouti","Denmark","Dominican Republic","Algeria","Ecuador","Egypt, Arab Rep.","Eritrea","Spain","Estonia","Ethiopia","Finland","Fiji","France","Micronesia, Fed. Sts.","Gabon","United Kingdom","Georgia","Ghana","Guinea","Gambia, The","Guinea-Bissau","Equatorial Guinea","Greece","Grenada","Greenland","Guatemala","Guam","Guyana","Hong Kong SAR, China","Honduras","Croatia","Haiti","Hungary","Indonesia","India","Ireland","Iran, Islamic Rep.","Iraq","Iceland","Israel","Italy","Jamaica","Jordan","Japan","Kazakhstan","Kenya","Kyrgyz Republic","Cambodia","Kiribati","Korea, Rep.","Kuwait","Lao PDR","Lebanon","Liberia","Libya","St. Lucia","Liechtenstein","Sri Lanka","Lesotho","Lithuania","Luxembourg","Latvia","Macao SAR, China","Morocco","Moldova","Madagascar","Maldives","Mexico","Macedonia, FYR","Mali","Malta","Myanmar","Montenegro","Mongolia","Mozambique","Mauritania","Mauritius","Malawi","Malaysia","Namibia","New Caledonia","Niger","Nigeria","Nicaragua","Netherlands","Norway","Nepal","New Zealand","Oman","Pakistan","Panama","Peru","Philippines","Papua New Guinea","Poland","Puerto Rico","Portugal","Paraguay","French Polynesia","Qatar","Romania","Russian Federation","Rwanda","Saudi Arabia","Sudan","Senegal","Singapore","Solomon Islands","Sierra Leone","El Salvador","Somalia","Serbia","South Sudan","Sao Tome and Principe","Suriname","Slovak Republic","Slovenia","Sweden","Swaziland","Seychelles","Syrian Arab Republic","Chad","Togo","Thailand","Tajikistan","Turkmenistan","Timor-Leste","Tonga","Trinidad and Tobago","Tunisia","Turkey","Tanzania","Uganda","Ukraine","Uruguay","United States","Uzbekistan","St. Vincent and the Grenadines","Venezuela, RB","Virgin Islands (U.S.)","Vietnam","Vanuatu","West Bank and Gaza","Samoa","Yemen, Rep.","South Africa","Congo, Dem. Rep.","Zambia","Zimbabwe")
+Regions_Dataset <- c("The Americas","Asia","Africa","Europe","Middle East","The Americas","Asia","The Americas","Oceania","Europe","Asia","Africa","Europe","Africa","Africa","Asia","Europe","Middle East","The Americas","Europe","Europe","The Americas","The Americas","The Americas","The Americas","The Americas","Asia","Asia","Africa","Africa","The Americas","Europe","The Americas","Asia","Africa","Africa","Africa","The Americas","Africa","Africa","The Americas","The Americas","The Americas","Europe","Europe","Europe","Africa","Europe","The Americas","Africa","The Americas","Africa","Africa","Europe","Europe","Africa","Europe","Oceania","Europe","Oceania","Africa","Europe","Asia","Africa","Africa","Africa","Africa","Africa","Europe","The Americas","The Americas","The Americas","Oceania","The Americas","Asia","The Americas","Europe","The Americas","Europe","Asia","Asia","Europe","Middle East","Middle East","Europe","Middle East","Europe","The Americas","Middle East","Asia","Asia","Africa","Asia","Asia","Oceania","Asia","Middle East","Asia","Middle East","Africa","Africa","The Americas","Europe","Asia","Africa","Europe","Europe","Europe","Asia","Africa","Europe","Africa","Asia","The Americas","Europe","Africa","Europe","Asia","Europe","Asia","Africa","Africa","Africa","Africa","Asia","Africa","Oceania","Africa","Africa","The Americas","Europe","Europe","Asia","Oceania","Middle East","Asia","The Americas","The Americas","Asia","Oceania","Europe","The Americas","Europe","The Americas","Oceania","Middle East","Europe","Europe","Africa","Middle East","Africa","Africa","Asia","Oceania","Africa","The Americas","Africa","Europe","Africa","Africa","The Americas","Europe","Europe","Europe","Africa","Africa","Middle East","Africa","Africa","Asia","Asia","Asia","Asia","Oceania","The Americas","Africa","Europe","Africa","Africa","Europe","The Americas","The Americas","Asia","The Americas","The Americas","The Americas","Asia","Oceania","Middle East","Oceania","Middle East","Africa","Africa","Africa","Africa")
+
+regions <- data.frame(cbind(Countries_Dataset,Regions_Dataset))
+View(regions)
+
+df <- merge(dbase,regions, by.x = "Country.Name", by.y = "Countries_Dataset")
+
+#changing a column name
+names(df)[6] <- "region"
+
+# changing labels and adding levels to categorical variables
+df$Income.Group <- factor(df$Income.Group, 
+                    levels = (c("Low income", 
+                             "Lower middle income", 
+                             "Upper middle income",
+                             "High income" )),
+                    labels = c("Low", "Low mid", "Up mid", "High"))
+
+levels(df$Income.Group)
+
+# task nr.1. change the region column to be a factor (Do not assign levels!)
+
+
+
+
+
+# Exploring your dataset -----------------
+?nrow()
+?ncol()
+?head
+?tail()
+?str()
+?summary()
+
+#### adding a column to data frame
+
+df$dummy.variable <- df$Internet.users > mean(df$Internet.users)
+
+
