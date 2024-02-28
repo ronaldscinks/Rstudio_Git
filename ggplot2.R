da04a3294f3e72d7795b2cf44cb79d879a80db39
Ronalds Cinks <ronalds.cinks@RBS.lan>
2024-02-28 17:09:26 +0200

    d

--- ggplot2.R
+++ ggplot2.R
@@ -1,53 +1,64 @@
 #ggplot2
 
-#install only if you have not installed it before
-install.packages("ggplot2")
+#1st layer define data 
+our.plot <- ggplot(df)
+our.plot
+# 2nd layer add aesthethics (mapping of x and y)
+our.plot <- our.plot + aes(Birth.rate, Internet.users)
+our.plot
+# 3rd layer add geom
+our.plot <- our.plot + geom_point()
+our.plot
+# 4th layer adding stats
+our.plot <- our.plot + stat_smooth()
+our.plot
+# 5th layer adding facets
+our.plot <- our.plot + facet_wrap(vars(Income.Group))
+our.plot
 
-#load the package everytime when you start a new session
-library(ggplot2)
+# 6th layer adding coordinates
+our.plot <- our.plot + coord_cartesian(xlim = c(5,50),
+                                       ylim = c(0,120))
+our.plot
 
-# load the movies data set into R studio
+# 7th layer adding theme
+our.plot <- our.plot + theme_classic()
+our.plot
 
-movies <- read.csv("Movie-Ratings.csv")
+#another example
+#1st layer define data 
+ggplot(df)
+# 2nd layer add aesthethics (mapping of x and y)
+ggplot(df, aes(Internet.users, region))
+# 3rd layer add geom
+ggplot(df, aes(Internet.users, region))+
+  geom_boxplot(aes(fill = region))
 
-
-# change the column names to shorter 
-
-names(movies)[c(3:6)] <- c("rotten_tomatos", "ratings", "budget", "year")
-
-#change the necessary variables to factors
-movies$Genre <- as.factor(movies$Genre)
-movies$year <- as.factor(movies$year)
-
-summary(movies)
-
-# What will we use?
-ggplot(data = movies, aes(budget, Genre, fill = Genre))+
-  geom_boxplot()
-
-ggplot(movies, aes(ratings, rotten_tomatos)) + 
-  geom_point(aes(size = budget))
-
-
-
-# What will we use?# What will we use?
-ggplot(data = mov, aes(x = , y = ))
-
-# Adding Geometry -------------------------------------
-# What will we use?
-ggplot(data = , aes(x = , y = )) + geom_----()
-
-
-?geom_abline()
-
-# Improving our plot ----------------------------------
-# Adding Color:
-ggplot(data = , aes(x = , y = , color = )) + geom_----()
+ggplot(df, aes(Internet.users, Birth.rate))+
+  geom_point(aes(color = region, size = Income.Group)) +
+  geom_smooth(se = F, color = "green")
 
 
-# Sizing:
-ggplot(data = , aes(x = , y = , color = , size = )) + geom_----()
+# 4th layer adding stats
+ggplot(df, aes(Internet.users, region))+
+  stat_summary(fun = "mean", geom = "bar")
 
-# This will be the CEO chart (to be improved)!
+# 5th layer adding facets
+ggplot(df, aes(Internet.users, region))+
+  geom_boxplot() +
+  facet_wrap(vars(Income.Group))
 
 
+# 6th layer adding theme
+ggplot(df, aes(Internet.users, 
+               reorder(region, Internet.users, decreasing = TRUE),
+               fill = region)) + 
+  geom_boxplot() +
+  theme_classic() +
+  theme(legend.position = "none") +
+  labs(title = "Internet users by Region")+
+  xlab("Internet users")+
+  ylab("Region")+
+  theme(plot.title = element_text(color = "blue", size = 30, family = "mono"),
+    axis.title.x = element_text(color = "DarkGreen", size = 20),
+        axis.title.y = element_text(color = "Red", size = 20))
